c6003618d42f76a8b1804c952815fb9c8bf93e4e
SatoruOsawa <osawa@takram.com>
2018-09-04 14:47:55 +0900

    add read

--- 00_simple/00_readable_characteristic/src/main.cpp
+++ 00_simple/00_readable_characteristic/src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include <BLEDevice.h>
+
+BLEServer * SERVER;
+BLEService * SERVICE;
+BLECharacteristic * CHARACTERISTIC;
+
+#define SERVICE_UUID "e77fe789-2d78-4123-be83-166486bb8b55"
+#define CHARACTERISTIC_UUID "ea1fb90b-6411-40e6-abca-e9b1b82350fa"
 
 void setup() {
-    // put your setup code here, to run once:
+  BLEDevice::init("ESP32_TEST");
+
+  SERVER = BLEDevice::createServer();
+  SERVICE = SERVER->createService(SERVICE_UUID);
+  CHARACTERISTIC = SERVICE->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ
+  );
+  SERVICE->start();
+  SERVER->getAdvertising()->start();
+
+  CHARACTERISTIC->setValue("Read");
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
 }
