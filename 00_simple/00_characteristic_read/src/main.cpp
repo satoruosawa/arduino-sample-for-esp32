b0ab88a66a0931d349756ae7a493e5d26b44c84f
SatoruOsawa <osawa@takram.com>
2018-09-04 15:30:02 +0900

    refactoring

--- 00_simple/00_characteristic_read/src/main.cpp
+++ 00_simple/00_characteristic_read/src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 #include <BLEDevice.h>
 
+#define SERVICE_UUID "e77fe789-2d78-4123-be83-166486bb8b55"
+#define CHARACTERISTIC_UUID "ea1fb90b-6411-40e6-abca-e9b1b82350fa"
+
 BLEServer * SERVER;
 BLEService * SERVICE;
 BLECharacteristic * CHARACTERISTIC;
-
-#define SERVICE_UUID "e77fe789-2d78-4123-be83-166486bb8b55"
-#define CHARACTERISTIC_UUID "ea1fb90b-6411-40e6-abca-e9b1b82350fa"
 
 void setup() {
   BLEDevice::init("ESP32_TEST");
