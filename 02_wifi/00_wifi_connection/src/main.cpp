da725eca62424e463d94064b748ddf807b9fa203
Satoru Osawa <osawa@takram.com>
2019-04-18 17:50:50 +0900

    Update wifi connection

--- 02_wifi/00_wifi_connection/src/main.cpp
+++ 02_wifi/00_wifi_connection/src/main.cpp
@@ -1,12 +1,10 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
 
 #include "./wifi-info.h"
 
 const char SSID[] = WIFI_SSID; // "xxxx"
 const char PASSWORD[] = WIFI_PASSWORD; // "xxxx"
-const char URL[] = SERVER_URL; // "http://xxx.xxx.xxx.xxx:8000/"
 
 void setup() {
   Serial.begin(115200);
@@ -26,18 +24,4 @@ void setup() {
 }
 
 void loop() {
-  HTTPClient http;
-  http.begin(URL);
-  String requestBody = "Hello World!";
-  int httpCode = http.POST(requestBody);
-
-  Serial.printf("Response: %d", httpCode);
-  Serial.println();
-  if (httpCode == HTTP_CODE_OK) {
-    String body = http.getString();
-    Serial.print("Response Body: ");
-    Serial.println(body);
-  }
-
-  delay(5000);
 }
